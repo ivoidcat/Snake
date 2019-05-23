d31ba09c0a1ea2aa69ad70c4c92024a07e7e8847
stephen <tasb00429@gmail.com>
2019-05-23 20:20:33 +0800

    [ Update ]：MainViewController

--- Snakee/DragMeToTheProject/is+Extension.swift
+++ Snakee/DragMeToTheProject/is+Extension.swift
@@ -1,14 +1,3 @@
 import Foundation
 import SpriteKit
 
-extension is {
-func gridTextureDontEat(_ view: Bool, title: String) {
-    UserDefaults.standard.setValue(view, forKey: "view")
-}
-func gridPositionDontWantLook(_ delegate: String, isPass: Bool) {
-    UserDefaults.standard.setValue(delegate, forKey: "delegate")
-}
-func isEmptyCannotLoud(_ delegate: Int, contents: Float, subtitle: String) {
-    UserDefaults.standard.setValue(delegate, forKey: "delegate")
-}
-}
